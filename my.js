d55731c5fecb81eaa7284eb4888d793d7fe6e895
Eren0414 <jagerkristof@ugmszi.hu>
2024-04-28 23:53:03 +0200

    valutavalto kesz

--- my.js
+++ my.js
@@ -10,7 +10,36 @@ createApp({
           { name: "Euró", symbol: "€", label: "EUR" },
           { name: "Orosz Rubel", symbol: "₽", label: "RUB" }
       ],
-      apiKey: ""
+      apiKey: "fca_live_BTlzHh78DJydSlUezWZ9gkLglkl9apDbWpixnygQ",
+      value: 1,
+      what: "HUF",
+      to: "HUF",
+      total: null
+    };
+  },
+  async mounted() {
+    await this.getCurrencyExchanges();
+    await this.onClickCounts();
+  },
+  methods: {
+    async getCurrencyExchanges(){
+      const response = await fetch(this.url);
+      const data  = await response.json();
+      this.currencyExchanges = data.data;
+    },
+    async onClickCounts(){
+      await this.getCurrencyExchanges();
+      this.total = this.value * this.currencyExchanges[this.to];
+    },
+  },
+  computed: {
+    url() {
+      return `https://api.freecurrencyapi.com/v1/latest?apikey=${this.apiKey}&currencies=${this.currenciesLabel}&base_currency=${this.what}`;
+    },
+    
+    currenciesLabel() {
+      return this.currencies.map((currencies) => currencies.label).join("%2C");
     }
-  }
+  },
+
 }).mount('#app')
